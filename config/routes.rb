c9f66122cd47e5820e935bb97448886c560cc1cd
Ubuntu <ubuntu@ip-172-31-13-45.us-west-1.compute.internal>
2014-08-29 05:52:25 +0000

    ng-boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks
-
+  resources :tasks, :except => [:new, :edit]
   resources :goals, :except => [:new, :edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
